75bf9514c48c516c1bb6548a7a2cb2e245a34973
copymachine <copymach@naver.com>
2021-12-09 22:22:19 +0900

    초밥

--- src/com/javaex/ex02/Ex01.java
+++ src/com/javaex/ex02/Ex01.java
@@ -32,5 +32,44 @@ public class Ex01 {
 		bout.close(); //bout.close(); 포함
 		
 	}
+	
+	
+	/*
+	  
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+
+	public static void main(String[] args) throws IOException{
+		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\img.jpg");
+		BufferedInputStream bin = new BufferedInputStream(in);
+		
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\bufferedimg.jpg");
+		BufferedOutputStream bout = new BufferedOutputStream(out);
+		
+		while(true) {
+			
+			int data = bin.read();
+			if(data == -1) {
+				System.out.println("복사가 완료 되었습니다." + data);
+				break;
+			}
+			
+			bout.write(data);
+		}
+		
+		
+		bin.close();
+		bout.close();
+	}
+	
+	 */
 
 }
